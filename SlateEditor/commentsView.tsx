490a4d0a3ffebb2cea83e16741593424eb8ef7e2
Mallesh Tirugudu <mallesh.tirugudu@accellor.com>
2026-02-16 23:53:36 +0530

    Update commentsView.tsx

--- SlateEditor/commentsView.tsx
+++ SlateEditor/commentsView.tsx
@@ -1,20 +1,7 @@
-const leafToInlineHtml = (node: any): string => {
-  if (!node || !("text" in node)) return "";
-  let html = node.text as string;
-  if (!html) return "";
-  if (node.code) html = `<code>${html}</code>`;
-  if (node.italic) html = `<em>${html}</em>`;
-  if (node.bold) html = `<strong>${html}</strong>`;
-  if (node.underline) html = `<u>${html}</u>`;
-  if (node.strikethrough) html = `<s>${html}</s>`;
-  return html;
-};
- 
-const childrenToInlineHtml = (children: any[]): string => {
-  return (children || [])
-    .map((c: any) => {
-      if ("text" in c) return leafToInlineHtml(c);
-      return childrenToInlineHtml(c.children);
-    })
-    .join("");
+const parseInlineHtml = (str: string): any[] => {
+  if (!str) return [{ text: "" }];
+  if (/<\/?[a-z][\s\S]*?>/i.test(str)) {
+    return htmlToLeaves(str);
+  }
+  return [{ text: str }];
 };
